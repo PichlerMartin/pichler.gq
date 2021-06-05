4cdc7b3a32a14de006e046bcfa4e23086a44383c
Martin Pichler <martin.pichler@enjoyit.at>
2021-06-05 21:02:14 +0200

    added de.vue

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-    ],
-  },
+  ]
 }
