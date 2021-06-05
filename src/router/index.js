2bc3afdf941774358e4467fb8b20c92e78b0e06b
Martin Pichler <martin.pichler@enjoyit.at>
2021-06-05 19:42:12 +0200

    added age

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/de',
+    name: 'de',
+    component: () => import('../../index.html'),
+  },
+  {
+    path: '/en',
+    name: 'en',
+    component: () => import('../../en/index.html'),
+  },
 ]
 
 const router = createRouter({
