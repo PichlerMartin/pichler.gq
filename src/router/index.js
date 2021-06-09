b6b1ec30dee3b762332ab8d37d93cc0e1b81f401
Martin Pichler <martin.pichler@enjoyit.at>
2021-06-09 09:01:28 +0200

    changed hihstory mode in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Lebenslauf from '../views/LebenslaufView.vue'
 import Resume from '../views/ResumeView.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
